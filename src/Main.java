68186a47326cda3557433c153a558c5e13d712e2
Your Name <f.dagani002@studenti.unibs.it>
2021-06-01 23:25:18 +0200

    sistematina, ma non funzia lo stesso

--- src/Main.java
+++ src/Main.java
@@ -9,14 +9,8 @@ public class Main {
 
     public static final String DIR_AR = "src/shide.jpg";
     public static final String DIR_IEE = "src/header_ieee.jpg";
-    public static final int DIMENSIONE = 100000;
 
     public static void main(String[] args) throws IOException {
-
-        BufferedImage img_ar = ImageIO.read(new URL("https://pgarnaldo.com/shide.jpg"));
-        BufferedImage img_ieee = ImageIO.read(new URL("https://ieeesb.unibs.it/images/header_ieee.jpg"));
-
-
 
         File fi_ar = new File(DIR_AR);
         byte[] fileContent_ar = Files.readAllBytes(fi_ar.toPath());
@@ -29,26 +23,26 @@ public class Main {
         int pos = 0;
         int i_ar =0;
         int i_iee = 0;
-        byte[] differenti = new byte[DIMENSIONE];
 
         while (i_ar < fileContent_ieee.length){
-            if(fileContent_ar[i_ar] != fileContent_ieee[i_iee]){
-                /*
-                differenti[pos] = fileContent_ar[i_ar];
-                System.out.println(differenti[pos] + "  " +i_ar);
-                 */
-                pos++;
-                /*
-                while (fileContent_ar[i_ar] != fileContent_ieee[i_iee]){
-                    i_iee++;
-                }
-
-                 */
-            }
+            if(fileContent_ar[i_ar] != fileContent_ieee[i_iee]) pos++;
             i_ar ++;
             i_iee ++;
         }
-        System.out.println(pos);
+        byte[] differenti = new byte[pos];
+        i_ar = 1275;
+        i_iee = 1275;
+        pos =0;
+
+        //XOR a casissimo
+        while (i_ar <fileContent_ieee.length){
+            differenti[pos] = (byte) (fileContent_ieee[i_iee] ^ fileContent_ar[i_ar]);
+            pos++;
+        }
+
+        System.out.println("");
+
+
         /*
         Metadata meta = new Metadata();
         String filename = "src/shide.jpg";
