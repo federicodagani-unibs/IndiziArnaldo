ed025e22cdc06968dd00b103ab7211832ad38571
Your Name <f.dagani002@studenti.unibs.it>
2021-06-07 17:09:36 +0200

    finalmente

--- src/Main.java
+++ src/Main.java
@@ -1,85 +1,48 @@
 import javax.imageio.ImageIO;
-import java.awt.*;
+import java.awt.Color;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
+import java.awt.image.WritableRaster;
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
 
 public class Main {
-
-    public static final String DIR_AR = "src/shide.jpg";
-    public static final String DIR_IEE = "src/header_ieee.jpg";
     public static final String DIR_CB_PESCA = "src/immagine_da_internet.png";
-    public static final String DIR_INDIZIO = "src/immagine_singola.jpg";
     public static final String DIR_CB_SCRIVI = "src/wrong_size.png";
 
     public static void main(String[] args) throws Exception {
 
-        /*
-        File fi_ar = new File(DIR_AR);
-        byte[] fileContent_ar = Files.readAllBytes(fi_ar.toPath());
-
-        File fi_ieee = new File(DIR_IEE);
-        byte[] fileContent_ieee = Files.readAllBytes(fi_ieee.toPath());
-         */
-
-        File cb_file = new File(DIR_CB_PESCA);
-        byte[] cb_byte_arr = Files.readAllBytes(cb_file.toPath());
-
-        /*
-        int dim = cb_byte_arr.length;
-        byte[] cb_byte_arr_mini = new byte[dim];
-        for(int i=0; i<dim; i++){
-            if(i<213 || i>4216){
-                cb_byte_arr_mini[i] = cb_byte_arr[i];
+        int width = 232, height = 232;
+        BufferedImage img_iniziale = ImageIO.read(new File(DIR_CB_PESCA));
+        WritableRaster wr = img_iniziale.getRaster() ;
+        //trovo la dimensione dell'array di pixel
+        int c=0;
+        for (int y=0 ; y < img_iniziale.getHeight() ; y++){
+            for (int x=0 ; x < img_iniziale.getWidth() ; x++){
+                c++;
             }
         }
-         */
-
-        ByteArrayInputStream bis = new ByteArrayInputStream(cb_byte_arr);
-        BufferedImage bImage2 = ImageIO.read(bis);
-        BufferedImage scaledImage = resizeImage(bImage2, 10, 250);
-        ImageIO.write(scaledImage, "png", new File(DIR_INDIZIO));
-        ImageIO.write(scaledImage, "png", new File(DIR_CB_SCRIVI) );
-        System.out.println("image resized");
-
-        BufferedImage qr_code_buffered = ImageIO.read(new File(DIR_CB_SCRIVI));
-        Graphics g = qr_code_buffered.createGraphics();
-        g.dispose();
-
-        BufferedImage qr_code_righe = new BufferedImage(250, 250, BufferedImage.TYPE_4BYTE_ABGR);
-        g = qr_code_righe.createGraphics();
-        for (int i=0; i<25; i++){
-            g.drawImage(qr_code_buffered, i*10,0, null);
+        //creo l'array
+        int x,y, i=0;
+        int[] pixels = new int[c];
+        for (y=0 ; y < img_iniziale.getHeight() ; y++){
+            for (x=0 ; x < img_iniziale.getWidth() ; x++){
+                pixels[i] = wr.getSample(x, y, 0) ;
+                i++;
+            }
         }
-        g.dispose();
-        ImageIO.write(qr_code_righe, "png", new File(DIR_CB_SCRIVI));
-        System.out.println("image duplicated");
-    }
-
-    public static BufferedImage resizeImage(BufferedImage originalImage, int targetWidth, int targetHeight) throws IOException {
-        BufferedImage resizedImage = new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_RGB);
-        Graphics2D graphics2D = resizedImage.createGraphics();
-        graphics2D.drawImage(originalImage, 0, 0, targetWidth, targetHeight, null);
-        graphics2D.dispose();
-        return resizedImage;
-    }
-
-    /*
-    public static BufferedImage createResizedCopy(Image originalImage, int scaledWidth, int scaledHeight, boolean preserveAlpha)
-    {
-        System.out.println("resizing...");
-        int imageType = preserveAlpha ? BufferedImage.TYPE_INT_RGB : BufferedImage.TYPE_INT_ARGB;
-        BufferedImage scaledBI = new BufferedImage(scaledWidth, scaledHeight, imageType);
-        Graphics2D g = scaledBI.createGraphics();
-        if (preserveAlpha) {
-            g.setComposite(AlphaComposite.Src);
+        //in base al valore dei pixel dell'array, setto nell'immagine finale(ridimensionata) il colore(in questo caso bianco o nero)
+        BufferedImage img_finale = new BufferedImage(width, height, BufferedImage.TYPE_4BYTE_ABGR);
+        i=0;
+        for(y=0; y<width; y++){
+            for(x=0; x<height; x++){
+                if(pixels[i] == 0){
+                    img_finale.setRGB(x,y, Color.black.getRGB());
+                }else {
+                    img_finale.setRGB(x,y,Color.white.getRGB());
+                }
+                i++;
+            }
         }
-        g.drawImage(originalImage, 0, 0, scaledWidth, scaledHeight, null);
-        g.dispose();
-        return scaledBI;
+        ImageIO.write(img_finale, "png", new File(DIR_CB_SCRIVI));
+        System.out.println("qr code creato :)");
     }
-
-     */
 }
